b0fbf8ea1701315b409b68e60c7bef5cd35b5080
VK <vkvinay@uvce.ac.in>
2021-09-04 05:28:22 +0530

    project setup

--- oct_crud/settings.py
+++ oct_crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qpn_j2gvg5ppx-5#rmx6vq@r!jys*^mxuwz4$*vcxnh8(9y+18
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'markdown',
+    'api',
+
 ]
 
 MIDDLEWARE = [
