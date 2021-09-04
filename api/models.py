de9734c0123466b8bd707c02f20f630a6eaaa29d
VK <vkvinay@uvce.ac.in>
2021-09-04 06:11:07 +0530

    override create and update with caluaclation

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Sum
 
 
 class Invoice(models.Model):
@@ -7,6 +8,12 @@ class Invoice(models.Model):
     total_quantity = models.IntegerField(default=0)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
+    def calc(self):
+        transactions = self.transactions.all().aggregate(Sum('quantity'), Sum('total'))
+        self.total_quantity = transactions['quantity__sum'] or 0
+        self.total_amount = transactions['total__sum'] or 0
+        self.save()
+
 
 class Transactions(models.Model):
     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name='transactions', null=True, blank=True)
@@ -14,4 +21,3 @@ class Transactions(models.Model):
     quantity = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-
--- api/serializers.py
+++ api/serializers.py
@@ -18,3 +18,38 @@ class InvoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = api.models.Invoice
         fields = '__all__'
+
+    def create(self, validated_data):
+        transactions = validated_data.pop('transactions', [])
+        invoice = api.models.Invoice.objects.create(**validated_data)
+        bulk_transactions = []
+        if transactions:
+            for transaction in transactions:
+                if 'id' in transaction:
+                    del transaction['id']
+                transaction.update({'invoice': invoice, 'total': transaction.get('quantity', 0) *
+                                    transaction.get('price', 0)})
+
+                bulk_transactions.append(api.models.Transactions(**transaction))
+            t = api.models.Transactions.objects.bulk_create(bulk_transactions)
+            invoice.calc()
+        return invoice
+
+    def update(self, instance, validated_data):
+        trans = validated_data.pop('transactions', [])
+        if not trans:
+            tran = instance.transactions.all()
+            tran.delete()
+        invoice = super(InvoiceSerializer, self).update(instance, validated_data)
+        create_transactions = []
+        if trans:
+            for transaction in trans:
+                transaction.update({'invoice': invoice, 'total': transaction.get('quantity', 0) *
+                                    transaction.get('price', 0)})
+                if 'id' in transaction:
+                    a, b = api.models.Transactions.objects.update_or_create(id=transaction['id'], defaults=transaction)
+                else:
+                    create_transactions.append(api.models.Transactions(**transaction))
+            api.models.Transactions.objects.bulk_create(create_transactions)
+        invoice.calc()
+        return invoice
